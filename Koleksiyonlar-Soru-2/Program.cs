ecc36e3c02a4c69e81c752733c5bb10de0cc5403
= <acikgoze18@itu.edu.tr>
2021-12-19 02:27:50 +0300

    Initial commit

---
+++ Koleksiyonlar-Soru-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Koleksiyonlar_Soru_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[20];
+            int[] smallNumbers = new int[3];
+            int[] bigNumbers = new int[3];
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("{0}. sayiyi girin",i+1);
+                var num = Convert.ToInt32(Console.ReadLine());
+                numbers[i] = num;
+            }
+            
+            Array.Sort(numbers);
+            for (int i = 0; i < 3; i++)
+            {
+                smallNumbers[i] = numbers[i];
+                bigNumbers[i] = numbers[numbers.Length-1-i];
+            }
+            
+            Console.WriteLine("En kucuk 3");
+            Array.ForEach(smallNumbers, (Console.WriteLine));
+            Console.WriteLine("En buyuk 3");
+            Array.ForEach(bigNumbers, (Console.WriteLine));
+
+        }
+    }
+}
---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sesliHarfler = new string[] {"a", "e", "i", "o", "u"};
+            string[] cumledekiSesliler = new string[5];
+            
+            Console.WriteLine("Cumle Girin: ");
+            string cumle = Console.ReadLine();
+
+            for(int i=0;i<sesliHarfler.Length; i++)
+            {
+                if (cumle.Contains(sesliHarfler[i]))
+                {
+                    cumledekiSesliler[i] = sesliHarfler[i];
+                }
+            }
+            Array.Sort(cumledekiSesliler);
+            Console.WriteLine("Sesli Harfler: ");
+            Array.ForEach(cumledekiSesliler,(Console.WriteLine));
+        }
+    }
+}
